86597ced99078e28e437932e5d81e5244f1cfe42
Mayara <mayaradepaula1997>
2024-04-13 18:05:14 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb2/course/resources/UserResource.java
+++ src/main/java/com/educandoweb2/course/resources/UserResource.java
@@ -1,19 +1,36 @@
 package com.educandoweb2.course.resources;
 
 import com.educandoweb2.course.entities.User;
+import com.educandoweb2.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping // resposta a uma requisição
-    public ResponseEntity<User> findAll (){
-        User u = new User(1L,"Maria","maria@gmail.com","999999","12345");
-        return ResponseEntity.ok().body(u);   //ResponseEntity.ok = retorna a resposta com sucesso no HTTP // body(u) = Corpo da resposta que foi instancio a classer "User"
+    public ResponseEntity<List<User>> findAll (){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);//ResponseEntity.ok = retorna a resposta com sucesso no HTTP // body(u) = Corpo da resposta que foi instancio a classer "User"
     }
+
+    //Implementar um Endpoint para buscar um usuario pelo Id
+    @GetMapping (value = "/{id}")
+    public ResponseEntity<User> findById (@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+
 
 }
