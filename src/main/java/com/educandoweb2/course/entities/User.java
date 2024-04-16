01a2d9ab78273fb2831402161780074539b5edcb
Mayara <mayaradepaula1997>
2024-04-15 22:14:33 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb2/course/entities/User.java
+++ src/main/java/com/educandoweb2/course/entities/User.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name="tb_user")
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
@@ -19,6 +21,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany (mappedBy = "client") //(mappedBy = "client"): nome do atributo que está do outro lado da associação/classe Order
+    private List <Order> orders = new ArrayList<>(); //Implementar a associação //Instanciar a coleção
 
     public User (){
 
@@ -70,6 +75,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order>getOrders(){
+        return orders;
     }
 
     @Override
